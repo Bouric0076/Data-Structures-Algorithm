d4d0bbae702857a6aceaf2f96f6f2710a5ddee3b
Bouric Enos <bouricenos0@gmail.com>
2025-05-13 19:41:16 +0300

    Deleted 1_quiz_1 in branch quiz_2

--- 1_quiz_1/single_number.py
+++
@@ -1,9 +0,0 @@
-def single_number(nums):
-    result = 0
-    for num in nums:
-        result ^= num
-    return result
-
-#Example
-nums = [4,1,2,1,2]
-print("The single number in the array is:", single_number(nums))
